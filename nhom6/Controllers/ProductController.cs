3bf676f96e3155d4561f2c0850492e16d69286c8
Lychee-tree <145662894+Lychee-tree@users.noreply.github.com>
2025-03-06 10:30:18 +0700

    Add project files.

---
+++ nhom6/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace nhom6.Frontend
+{
+    public class ProductController : Controller
+    {
+        private csdl db= new csdl();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            var listProduct = db.Products.ToList();
+            return View(listProduct);
+        }
+    }
+}
